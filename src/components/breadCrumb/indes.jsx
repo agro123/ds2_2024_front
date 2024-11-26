43c37507ceb7ab5a1428462a60362c63ead4d084
alejandro <alejandrovillamilcarmona@gmail.com>
2024-11-25 19:29:45 -0500

    Breadcrumb and loguot

---
+++ src/components/breadCrumb/indes.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Breadcrumb , Tooltip} from 'antd';
+import { HomeOutlined, RightOutlined } from '@ant-design/icons';
+import { Link, useLocation } from 'react-router-dom';
+
+// Función para transformar un segmento de ruta en un nombre legible
+const capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
+
+const BreadCrumb = () => {
+    const location = useLocation();
+
+    // Divide la ruta actual en segmentos
+    const pathSnippets = location.pathname.split('/').filter((i) => i);
+
+    // Acumula el path para cada segmento
+    const breadcrumbItems = pathSnippets.map((_, index) => {
+        const url = `/${pathSnippets.slice(0, index + 1).join('/')}`;
+        const currentPath = pathSnippets[index];
+
+        const breadcrumbNameMap = {
+            inicio: 'Inicio',
+            pqrsd: 'PQRSD',
+            lista: 'Lista de PQRSD',
+        };
+
+        return {
+            title: breadcrumbNameMap[currentPath] || capitalize(currentPath),
+            path: url,
+        };
+    });
+
+    // Agrega un elemento raíz "Inicio" si no está presente
+    const firstBreadcrumb = {
+        title: 'Inicio',
+        path: '/',
+    };
+
+    const fullBreadcrumbItems = [firstBreadcrumb, ...breadcrumbItems];
+
+    return (
+        <div className="breadcrumb-container">           
+            <Breadcrumb
+                items={fullBreadcrumbItems.map((item, index) => {
+                    // Determinar si es el último elemento
+                    const isLast = index === fullBreadcrumbItems.length - 1;
+
+                    return {
+                        key: item.key,
+                        href: isLast ? undefined : item.href,
+                        title: isLast ? (
+                            <span className="breadcrumb-current">{item.title}</span>
+                        ) : (
+                            <Tooltip title={typeof item.title === 'string' ? item.title : ''}>
+                                <Link to={item.href} className="breadcrumb-link" style={{ color: '#1890ff' }}>
+                                    {item.title}
+                                </Link>
+                            </Tooltip>
+                        ),
+                    };
+                })}
+                separator={<RightOutlined style={{ color: '#1890ff', fontSize: '10px' }} />}
+                style={{ padding: '.2rem .2rem', borderBottom: '1px solid gray', margin: '1rem 0', width: 'max-content' }}
+            />
+        </div>
+    );
+};
+
+export default BreadCrumb;
--- src/layouts/sidebar/index.jsx
+++ src/layouts/sidebar/index.jsx
@@ -1,11 +1,14 @@
-import { Menu } from "antd";
+import { Menu, Avatar, Button } from "antd";
 import Sider from "antd/es/layout/Sider";
 import {
     DesktopOutlined,
     PieChartOutlined,
+    UserOutlined,
+    LogoutOutlined
 } from '@ant-design/icons';
 import { useState } from "react";
 import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthProvider';
 
 function getItem(label, key, icon, children, path) {
     return {
@@ -19,6 +22,7 @@ function getItem(label, key, icon, children, path) {
 
 export const SideBar = () => {
     const navigate = useNavigate(); // Hook para navegar programáticamente
+    const { userData, clearSavedData } = useAuth();
 
     const items = [
         getItem(<Link to="/admin/dashboard">Dashboard</Link>, '1', <PieChartOutlined />),
@@ -28,22 +32,71 @@ export const SideBar = () => {
 
     const [collapsed, setCollapsed] = useState(false);
 
+    // Función para manejar el cierre de sesión
+    const handleLogout = () => {
+        clearSavedData();
+        navigate('/login'); // Redirige al usuario a la página de login
+    };
+
     return (
-        <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-            <div className="demo-logo-vertical" />
-            <Menu 
-                theme="dark" 
-                defaultSelectedKeys={['1']} 
-                mode="inline" 
-                items={items} 
-                onClick={({ key }) => {
-                    const item = items.find(item => item.key === key || item.children?.some(child => child.key === key));
-                    if (item) {
-                        const target = item.path || item.children?.find(child => child.key === key)?.path;
-                        if (target) navigate(target);
-                    }
-                }}
-            />
-        </Sider>
+        <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)} >
+            <section style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+                <div className="sidebar-header">
+                    <div style={{ textAlign: 'center', padding: '3rem 0' }}>
+                        <Avatar
+                            size="large"
+                            icon={<UserOutlined />}
+                            style={{ backgroundColor: '#1890ff' }}
+                        />
+                        
+                            <div className="user-info">
+                                <h4 style={{ textAlign: 'center', color: 'white', marginTop: '.5rem' }}>{!collapsed && userData && (userData.name)}</h4>
+                            </div>
+                        
+                        <div style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center'
+                        }}>
+                            <Button
+                                type="text"
+                                icon={<LogoutOutlined />}
+                                style={{
+                                    width: '50%',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    color: '#ffffff',
+                                    fontSize: '14px',
+                                    padding: '0px 8px',
+                                    borderRadius: '4px',
+                                    background: 'red'
+                                }}
+                                onClick={handleLogout}
+                            >
+                                {!collapsed && "Salir"}
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+                <Menu
+                    theme="dark"
+                    defaultSelectedKeys={['1']}
+                    mode="inline"
+                    items={items}
+                    onClick={({ key }) => {
+                        const item = items.find(item => item.key === key || item.children?.some(child => child.key === key));
+                        if (item) {
+                            const target = item.path || item.children?.find(child => child.key === key)?.path;
+                            if (target) navigate(target);
+                        }
+                    }}
+                />
+                {/* Botón de cierre de sesión */}
+                <div style={{ height: '100%', display: 'flex', alignItems: 'flex-end' }}>
+
+                </div>
+            </section>
+
+        </Sider >
     );
 };
